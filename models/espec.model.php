5d226000e2dd130bfe5f90ae3f4e68c4bfb7d542
Joel Dicharry <joeldicharry19@gmail.com>
2019-10-14 19:32:48 -0300

    armado de join

--- models/espec.model.php
+++ models/espec.model.php
@@ -6,9 +6,16 @@ class ModelEsp {
     public function __construct() {
         $this->db = new PDO('mysql:host=localhost;dbname=escuela;charset=utf8', 'root', '');
     }
+
     public function addEspec($nombre_esp){
         var_dump($nombre_esp);
         $query = $this->db->prepare('INSERT  INTO especialidad(nombre_esp) VALUES(?)');
         $query->execute([$nombre_esp]);
+    }
+
+    public function getConGenero($idpelicula){
+        $query = $this->db->prepare('SELECT alumno.nombre, alumno.apellido, alumno.dni AS especialidad FROM alumno JOIN especialidad ON alumno.id_especialidad=especialidad.nombre WHERE alumno.id_alumno=?');
+        $query->execute([$idpelicula]);
+        return $query->fetch(PDO::FETCH_OBJ);
     }
 }
