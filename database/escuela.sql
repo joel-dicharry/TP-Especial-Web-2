7c097616f4e9045a27a50463d5e6fd392d12467e
Benjahoff <oriozabalabenja@gmail.com>
2019-11-27 01:16:29 -0300

    database

--- database/escuela.sql
+++ database/escuela.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 27-11-2019 a las 05:13:10
+-- Tiempo de generación: 27-11-2019 a las 05:15:59
 -- Versión del servidor: 10.1.40-MariaDB
 -- Versión de PHP: 7.3.5
 
