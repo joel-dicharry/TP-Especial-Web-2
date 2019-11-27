cb1c0702cdcfa098e46fb9903816aced77d65c1e
joel-dicharry <joeldicharry19@gmail.com>
2019-11-27 07:50:35 -0300

    registro_

--- controllers/login.controller.php
+++ controllers/login.controller.php
@@ -31,6 +31,7 @@ class LoginController {
             $this->view->showLogin();
         }
     }
+
     public function createUser() {
         $usuario = $_POST['username'];
         $email = $_POST['email'];
